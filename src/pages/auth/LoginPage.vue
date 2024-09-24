91cc4efdc55cccf1c753bf4668f3a7c63bf13e21
ParkJeongBin01 <areoin@naver.com>
2024-09-24 15:44:07 +0900

    feat_Login

--- src/pages/auth/LoginPage.vue
+++ src/pages/auth/LoginPage.vue
@@ -7,7 +7,6 @@ const route = useRoute();
 const router = useRouter();
 const auth = useAuthStore();
 
-//////////////////////////////////////////////////////////
 const member = reactive({
   id: '',
   password: '',
@@ -33,7 +32,7 @@ const login = async () => {
     error.value = e.response.data;
   }
 };
-//////////////////////////////////////////////////////////
+
 </script>
 
 <template>
@@ -41,9 +40,9 @@ const login = async () => {
   <body class="bg-light">
     <main class="page-wrapper">
       <div class="container-fluid d-flex h-100 align-items-center justify-content-center py-4 py-sm-5">
-        <div class="card card-body" style="max-width: 940px">
+        <div class="card card-body" style="max-width: 940px; height: 670px;">
           <a class="position-absolute top-1 end-0 nav-link fs-sm py-1 px-2 me-3" href="#" onclick="window.history.go(-1); return false;">
-            <i class="fi-arrow-long-left fs-base me-2"></i>Go back</a>
+            <i class="fa-solid fa-arrow-left fs-base me-2"></i>Go back</a>
               <div class="row mx-0 align-item-center">
                 <div class="col-md-6 border-end-md p-2 p-sm-1">
                   <div>
@@ -53,17 +52,14 @@ const login = async () => {
                   <img src="@/assets/images/Loginimg.png" style="height: 100%; max-height: 60%;"> </img>
                     <div class="mt-4 mt-sm-5">
                     계정이 없으신가요??
-                    <a href="#">회원가입</a>
+                    <a href="#" style="color: #ff8f17;">회원가입</a>
                     </div>
                 </div>
 
 <!-- ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ오른쪽 화면ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ -->
-
         <div class="col-md-6 px-2 pt-2 pb-4 px-sm-5 pb-sm-5 pt-md-5">
           <a class="btn btn-outline-info w-100 mb-3" href="#">
-            <i class="fi-google fs-lg me-1">
-              ::before
-            </i>
+            <i class="fa-brands fa-google fs-lg me-1"></i>
               Sign in with Google
           </a>
           <div class="d-flex align-items-center py-3 mb-3">
@@ -79,19 +75,13 @@ const login = async () => {
                 <div class="mb-4">
                   <div class="d-flex align-items-center justify-content-between mb-2">
                     <label class="form-label mb-0" for="signin-password">Password</label>
-                    <a class="fs-ms" href="#">Forgot Password?</a>
+                    <a class="fs-ms" href="#" style="color: #ff8f17;">Forgot Password?</a>
                   </div>
-                  <div class="password-toggle">
+                  <div class="change">
                     <input class="form-control" type="password" id="signin-password" placeholder="Enter Password" required>
-                    <label class="password-toggle-btn" aria-label="Show/hide password">
-                      <input class="password-toggle-check" type="checkbox">
-                      <span class="password-toggle-indicator">
-                        ::before
-                      </span>
-                    </label>
                   </div>  
                 </div>
-                  <button class="btn btn-primary btn-lg w-100" type="submit">Sign in</button>
+                  <button class="btn-orange btn-lg w-100" type="submit">Sign in</button>
             </form>
         </div>
             </div>
@@ -99,36 +89,4 @@ const login = async () => {
       </div>
     </main>
   </body>
-
-  <!-- <div class="mt-5 mx-auto" style="width: 500px">
-    <h1 class="my-5">
-      <i class="fa-solid fa-right-to-bracket"></i>
-      로그인
-    </h1>
-
-    <form @submit.prevent="login">
-      <div class="mb-3 mt-3">
-        <label for="id" class="form-label">
-          <i class="fa-solid fa-user"></i>
-          사용자 ID:
-        </label>
-        <input type="text" class="form-control" placeholder="사용자 ID" v-model="member.id" required />
-      </div>
-
-      <div class="mb-3">
-        <label for="password" class="form-label">
-          <i class="fa-solid fa-lock"></i>
-          비밀번호:
-        </label>
-        <input type="password" class="form-control" placeholder="비밀번호" v-model="member.password" required />
-      </div>
-
-      <div v-if="error" class="text-danger">{{ error }}</div>
-
-      <button type="submit" class="btn btn-primary mt-4" :disabled="disableSubmit">
-        <i class="fa-solid fa-right-to-bracket"></i>
-        로그인
-      </button>
-    </form>
-  </div> -->
 </template>
--- src/pages/board/BoardListPage.vue
+++ src/pages/board/BoardListPage.vue
@@ -41,46 +41,6 @@ const pageRequest = reactive({
 // 페이지네이션 페이지 변경
 const handlePageChange = async (pageNum) => {
   router.push({
-<<<<<<< HEAD
-    query: { page: pageNum, amount: pageRequest.amount, searchType: pageRequest.searchType, searchValue: pageRequest.searchValue, types: pageRequest.types },
-  });
-};
-
-// 검색어 변경
-const searchChange = async () => {
-  console.log('CLICK,,,,');
-  // url 변경 --> query 파트만 변경되므로 컴포넌트가 다시 마운트되지 않음
-  // watch를 통해 cr이 변경됨을 감지하여 페이지 로드해야 함
-  router.push({
-    query: { page: pageRequest.page, amount: pageRequest.amount, searchType: pageRequest.searchType, searchValue: pageRequest.searchValue, types: pageRequest.types },
-  });
-};
-
-// pageRequest의 값 변경된 경우 호출
-watch(route, async (newValue) => {
-  console.log('WATCH', route.query.page);
-  await load(route.query);
-});
-
-const load = async (query) => {
-  try {
-    page.value = await api.getList(query);
-    console.log(page.value);
-
-    if (pageRequest.types.length == 0) {
-      page.value.boardCategory.forEach((element) => {
-        pageRequest.types.push(element.type);
-      });
-    }
-  } catch {}
-};
-
-load(pageRequest);
-
-const typeChange = (evant) => {
-  alert(evant.target.checked);
-};
-=======
     query: {
       page: pageNum,
       amount: pageRequest.amount,
@@ -90,8 +50,6 @@ const typeChange = (evant) => {
     },
   });
 };
-
->>>>>>> 9e7c2e997859f873ea94ab5cb9347453c1edd911
 </script>
 
 <template>
@@ -99,102 +57,17 @@ const typeChange = (evant) => {
     <h1 class="mb-3">공지 사항</h1>
 
     <div class="mt-4">
-<<<<<<< HEAD
-      <div class="form-label">type</div>
-      <div v-for="item in page.boardCategory" :key="item.type" class="form-check form-check-inline">
-        <input v-model="pageRequest.types" checked class="form-check-input" type="checkbox" :id="item.type" :value="item.type" />
-        <label class="form-check-label" :for="item.type">{{ item.name }}</label>
-      </div>
-    </div>
-
-    <div class="row mt-2">
-      <div class="col-3">
-        <label class="form-label">검색 종류</label>
-        <select v-model="pageRequest.searchType" class="form-select" required>
-          <option value="">선택하세요.</option>
-          <option value="all">전체</option>
-          <option value="title">제목</option>
-          <option value="content">내용</option>
-          <option value="writer">글쓴이</option>
-        </select>
-      </div>
-      <div class="col-8">
-        <label class="form-label">검색어</label>
-        <input v-model="pageRequest.searchValue" v-on:keyup.enter="searchChange" type="text" class="form-control" placeholder="내용을 입력하세요." />
-      </div>
-      <div class="col-1">
-        <label class="form-label"> &nbsp <span class="text-body-secondary"></span></label>
-        <button class="form-control btn btn-primary" @click="searchChange">검색</button>
-      </div>
-    </div>
-
-    <div class="mt-3 text-end">(총 {{ page.totalCount }}건)</div>
-
-    <table class="table table-striped">
-      <thead>
-        <tr>
-          <th style="width: 60px">No</th>
-          <th style="width: 100px">Type</th>
-          <th>제목</th>
-          <th style="width: 150px">작성자</th>
-          <th style="width: 120px">작성일</th>
-          <th style="width: 80px">조회수</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="article in articles" :key="article.bno">
-          <td>{{ article.bno }}</td>
-          <td>
-            {{
-              page.boardCategory.filter((value) => {
-                if (value.type == article.type) {
-                  return article.type;
-                }
-              })[0].name
-            }}
-          </td>
-          <td>
-            <router-link :to="{ name: 'board/detail', params: { no: article.bno }, query: route.query }"> {{ article.title }} </router-link>
-          </td>
-          <td>{{ article.memberName }}({{ article.memberId }})</td>
-          <td>{{ moment(article.regDate).format('YYYY-MM-DD') }}</td>
-          <td>{{ article.readCount }}</td>
-        </tr>
-      </tbody>
-    </table>
-
-    <div class="my-5 d-flex">
-      <div class="flex-grow-1 text-center">
-        <!-- 페이지 네이션 -->
-        <vue-awesome-paginate
-          :total-items="page.totalCount"
-          :items-per-page="pageRequest.amount"
-          :max-pages-shown="5"
-          :show-ending-buttons="true"
-          v-model="pageRequest.page"
-          @click="handlePageChange"
-        >
-          <template #first-page-button><i class="fa-solid fa-backward-fast"></i></template>
-          <template #prev-button><i class="fa-solid fa-caret-left"></i></template>
-          <template #next-button><i class="fa-solid fa-caret-right"></i></template>
-          <template #last-page-button><i class="fa-solid fa-forward-fast"></i></template>
-        </vue-awesome-paginate>
-      </div>
-      <div>
-        <router-link :to="{ name: 'board/create', query: route.query }" class="btn btn-primary"><i class="fa-solid fa-pen-to-square"></i> 글 작성</router-link>
-      </div>
-=======
       <div class="form-label">새로운 소식과 공지를 확인하세요!</div>
       <!-- 공지 사항을 article로 구성 -->
       <div class="row row-cols-md-2 row-cols-1 gy-md-5 gy-4 mb-lg-5 mb-4">
         <article class="col pb-2 pb-md-1" v-for="item in articles" :key="item.id">
           <a class="d-block position-relative mb-3" :href="`/board/${item.id}`">
-            <img class="d-block rounded-3"
-                 :src="item.imageUrl || 'https://image.ajunews.com/content/image/2022/04/04/20220404181310254680.jpg'"
-                 alt="공지 이미지">
+            <img class="d-block rounded-3" :src="item.imageUrl || 'https://image.ajunews.com/content/image/2022/04/04/20220404181310254680.jpg'" alt="공지 이미지" />
           </a>
           <a class="fs-sm text-uppercase text-decoration-none" href="#">{{ item.title }}</a>
-          <h3 class="h5 mb-2 pt-1"><a class="nav-link" :href="`/board/${item.id}`">{{ item.title }}</a></h3>
+          <h3 class="h5 mb-2 pt-1">
+            <a class="nav-link" :href="`/board/${item.id}`">{{ item.title }}</a>
+          </h3>
           <p class="mb-3">{{ item.content }}</p>
           <a class="d-flex align-items-center text-decoration-none" :href="`/board/${item.id}`">
             <!--            <button class="btn btn-primary">자세히 보기</button>-->
@@ -205,14 +78,19 @@ const typeChange = (evant) => {
 
     <!-- 페이지네이션 -->
     <div class="my-5 d-flex justify-content-center">
-      <vue-awesome-paginate :total-items="page.totalCount" :items-per-page="pageRequest.amount" :max-pages-shown="5"
-                            :show-ending-buttons="true" v-model="pageRequest.page" @click="handlePageChange">
+      <vue-awesome-paginate
+        :total-items="page.totalCount"
+        :items-per-page="pageRequest.amount"
+        :max-pages-shown="5"
+        :show-ending-buttons="true"
+        v-model="pageRequest.page"
+        @click="handlePageChange"
+      >
         <template #first-page-button><i class="fa-solid fa-backward-fast"></i></template>
         <template #prev-button><i class="fa-solid fa-caret-left"></i></template>
         <template #next-button><i class="fa-solid fa-caret-right"></i></template>
         <template #last-page-button><i class="fa-solid fa-forward-fast"></i></template>
       </vue-awesome-paginate>
->>>>>>> 9e7c2e997859f873ea94ab5cb9347453c1edd911
     </div>
   </div>
 </template>
